68511cb5746e71e6c622a1a8feaa381e0acd85c3
timeking23 <danielgetahun46@gmail.com>
2024-02-20 09:48:38 -0600

    create dir and print to txt

--- system_info/system_info.sh
+++ system_info/system_info.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+mkdir system_info
+cd ./system_info 
+touch system_info.txt
+
 echo "System information has been gathered.  Here’s the summary: "
 echo "date: " $(date +"%x %X %Z")
 echo "user:" $(whoami)
@@ -5,3 +10,5 @@ echo "Current Working Directory: " $(pwd)
 echo -e "System Usage: \n" $(top -b -i -n1)
 echo
 echo "Disk Usage: " && df 
+
+cat system_info.sh > system_info.txt
